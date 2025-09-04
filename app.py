6628e50196a4fb21bedfb5f27730691267cf449b
Andrea Mantovani <andreamnt94@gmail.com>
2025-09-04 23:34:44 +0200

    app.py - Added support for multiview Removed webcam and clipboard from image source

--- app.py
+++ app.py
@@ -9,28 +9,58 @@ import tempfile
 temp_dir = tempfile.gettempdir()
 mesh_format = 'glb'
 
-pipeline = Hunyuan3DDiTFlowMatchingPipeline.from_pretrained(
+pipeline_mv = Hunyuan3DDiTFlowMatchingPipeline.from_pretrained(
+    'tencent/Hunyuan3D-2mv',
+    subfolder='hunyuan3d-dit-v2-mv',
+    variant='fp16'
+)
+
+pipeline_default = Hunyuan3DDiTFlowMatchingPipeline.from_pretrained(
     'tencent/Hunyuan3D-2',
     subfolder='hunyuan3d-dit-v2-0',
     variant='fp16'
 )
 
-def process_image(image_path, seed):
-    image = Image.open(image_path).convert("RGBA")
-    if image.mode == 'RGB':
-        rembg = BackgroundRemover()
-        image = rembg(image)
-    
-    mesh = pipeline(
-        image=image,
-        num_inference_steps=50,
-        octree_resolution=380,
-        num_chunks=20000,
-        generator=torch.manual_seed(seed),
-        output_type='trimesh'
+def process_image_test(font_image_path, left_image_path, back_image_path, seed):
+    use_multiview = all([font_image_path, left_image_path, back_image_path])
+
+    if use_multiview:
+        images = {
+            "front": font_image_path,
+            "left": left_image_path,
+            "back": back_image_path
+        }
+        for key in images:
+            image = Image.open(images[key]).convert("RGBA")
+            if image.mode == 'RGB':
+                rembg = BackgroundRemover()
+                image = rembg(image)
+            images[key] = image
+
+        mesh = pipeline_mv(
+            image=images,
+            num_inference_steps=50,
+            octree_resolution=380,
+            num_chunks=20000,
+            generator=torch.manual_seed(seed),
+            output_type='trimesh'
         )[0]
-    
-    mesh_name = os.path.splitext(os.path.basename(image_path))[0]
+    else:
+        image = Image.open(font_image_path).convert("RGBA")
+        if image.mode == 'RGB':
+            rembg = BackgroundRemover()
+            image = rembg(image)
+
+        mesh = pipeline_default(
+            image=image,
+            num_inference_steps=50,
+            octree_resolution=380,
+            num_chunks=20000,
+            generator=torch.manual_seed(seed),
+            output_type='trimesh'
+        )[0]
+
+    mesh_name = os.path.splitext(os.path.basename(font_image_path))[0]
     temp_mesh_path = os.path.join(temp_dir, f'{mesh_name}-{seed}.{mesh_format}')
 
     print(f"Exporting mesh to {temp_mesh_path}...")
@@ -40,12 +70,22 @@ def process_image(image_path, seed):
 with gr.Blocks() as demo:
     with gr.Row():
         with gr.Column(scale=1):
-            image_input = gr.Image(type="filepath", label="Input Image")
-            seed_input = gr.Slider(10000, 100000, step=1, label="Seed", value=10042)
-            process_button = gr.Button("Generate")
+            with gr.Row():
+                front_image_input = gr.Image(type="filepath", label="Front Image", sources=["upload"])
+            with gr.Row():
+                left_image_input = gr.Image(type="filepath", label="Left Image", sources=["upload"])
+                back_image_input = gr.Image(type="filepath", label="Back Image", sources=["upload"])
+            with gr.Row():
+                seed_input = gr.Slider(10000, 100000, step=1, label="Seed", value=10042)
+            with gr.Row():
+                process_button = gr.Button("Generate")
         with gr.Column(scale=4, min_width=600):
             model_output = gr.Model3D(label="3D Mesh Reconstruction", clear_color=[0.0, 0.0, 0.0, 0.0], height=800)
 
-    process_button.click(fn=process_image, inputs=[image_input, seed_input], outputs=model_output)
+    process_button.click(
+        fn = process_image_test,
+        inputs = [front_image_input, left_image_input, back_image_input, seed_input],
+        outputs = model_output
+    )
 
 demo.launch(inbrowser=True)
